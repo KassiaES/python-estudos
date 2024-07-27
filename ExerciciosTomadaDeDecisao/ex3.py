f6e9c1bef92eb55e93a11744260e6b75ef139bd3
Kassia do Espirito Santo <kassiaes@gmail.com>
2024-07-27 17:20:44 -0300

    exercício tomada de decisão 3

--- ExerciciosTomadaDeDecisao/ex3.py
+++ ExerciciosTomadaDeDecisao/ex3.py
@@ -3,3 +3,12 @@ Faça um programa que peça uma nota, entre zero e dez. Mostre uma
 mensagem caso o valor seja inválido e continue pedindo até que o usuário
 informe um valor válido.
 '''
+
+nota = -1
+
+while nota < 0 or nota > 10:
+    nota = float(input("Digite o valor da nota (entre 0 e 10): "))
+    if nota < 0 or nota > 10:
+        print("Valor inválido! Por favor, insira uma nota entre 0 e 10.")
+
+print(f"Nota válida recebida: {nota}")
