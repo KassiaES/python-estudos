27cfdd849072cc79fff493c16c8f2290ec566a4a
Kassia do Espirito Santo <kassiaes@gmail.com>
2024-07-27 19:03:03 -0300

    exercício tomada de decisão 4

--- ExerciciosTomadaDeDecisao/ex4.py
+++ ExerciciosTomadaDeDecisao/ex4.py
@@ -4,3 +4,12 @@ pontuação em um exame. O programa deverá solicitar uma nota de 0 a 10. Se
 a pontuação for maior ou igual a 7, o aluno é aprovado; caso contrário, é
 reprovado
 '''
+
+nota = float(input('Digite a sua nota de 0 a 10: '))
+
+if nota >= 7 and nota <= 10:
+    print('Aprovado!')
+elif nota < 7 and nota >=0:
+    print('Reprovado..')
+else:
+    print("Valor Inválido!")
