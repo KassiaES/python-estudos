b72d00f75bfeb41951c19472a327a83b818426c3
Kassia do Espirito Santo <kassiaes@gmail.com>
2024-07-27 20:14:01 -0300

    exercício tomada de decisão 7

--- ExerciciosTomadaDeDecisao/ex7.py
+++ ExerciciosTomadaDeDecisao/ex7.py
@@ -2,3 +2,16 @@
 Desenvolver um programa que solicite a idade do usuário e identifique se
 ele é uma criança, um adolescente, adulto ou idoso.
 '''
+
+idade = int(input('Digite a sua idade: '))
+
+if (idade > 0 and idade <= 12):
+    print('Você é uma criança')
+elif (idade > 12 and idade < 18):
+    print('Você é um(a) adolescente')
+elif (idade > 17 and idade < 60):
+    print('Você é um(a) adulto(a)')
+elif (idade > 59 and idade <= 105):
+    print('Você é um(a) idoso(a)')
+else:
+    print('Valor inválido')
