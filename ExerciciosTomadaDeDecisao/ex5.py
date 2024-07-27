af3c7546da8fdc17d86fffa99580b8275a6d0bfd
Kassia do Espirito Santo <kassiaes@gmail.com>
2024-07-27 19:25:52 -0300

    exercício tomada de decisão 5

--- ExerciciosTomadaDeDecisao/ex5.py
+++ ExerciciosTomadaDeDecisao/ex5.py
@@ -5,3 +5,14 @@ equilátero: todos os lados com o mesmo valor
 isósceles: dois lados com o mesmo valor
 escaleno: todos os lados com medidas distintas.
 '''
+
+lado1 = int(input('Digite o comprimento do primeiro lado do triângulo: '))
+lado2 = int(input('Digite o comprimento do segundo lado do triângulo: '))
+lado3 = int(input('Digite o comprimento do terceiro lado do triângulo: '))
+
+if lado1 == lado2 == lado3:
+    print('Triângulo equilátero')
+elif lado1 == lado2 or lado1 == lado3 or lado2 == lado3:
+    print('Triângulo isósceles')
+else:
+    print('Triângulo escaleno')
