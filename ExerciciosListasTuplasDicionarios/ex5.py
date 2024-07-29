4ad9ce2dd0da3e9d3f785fe82dce6483545e017c
Kassia do Espirito Santo <kassiaes@gmail.com>
2024-07-29 10:41:23 -0300

    ajustes

--- ExerciciosListasTuplasDicionarios/ex5.py
+++ ExerciciosListasTuplasDicionarios/ex5.py
@@ -5,16 +5,26 @@ Crie duas tuplas. Concatene-as para formar uma nova tupla.
 elementos1 = []
 print('Digite 3 números inteiros para a primeira tupla:')
 for i in range(3):
-    numero = int(input(f'Digite o elemento {i + 1}: '))
-    elementos1.append(numero)
+    while True:
+        try:
+            numero = int(input(f'Digite o elemento {i + 1}: '))
+            elementos1.append(numero)
+            break  # Sai do loop se a entrada for válida
+        except ValueError:
+            print('Por favor, insira um número inteiro válido.')
 
 tupla1 = tuple(elementos1)
 
 elementos2 = []
 print('Digite 3 números inteiros para a segunda tupla:')
-for i in range(3): 
-    numero = int(input(f'Digite o elemento {i + 1}: '))
-    elementos2.append(numero)
+for i in range(3):
+    while True:
+        try:
+            numero = int(input(f'Digite o elemento {i + 1}: '))
+            elementos2.append(numero)
+            break  # Sai do loop se a entrada for válida
+        except ValueError:
+            print('Por favor, insira um número inteiro válido.')
 
 tupla2 = tuple(elementos2)
 
@@ -22,5 +32,4 @@ tupla_concatenada = tupla1 + tupla2
 
 print("Primeira tupla:", tupla1)
 print("Segunda tupla:", tupla2)
-
 print("Tupla concatenada:", tupla_concatenada)
