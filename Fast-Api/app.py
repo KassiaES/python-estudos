f4aae736ea8105860b4bdae9c6719b40979fe724
Kassia ES <kassiaes@gmail.com>
2024-09-02 14:44:56 -0300

    add put on fast-api

--- Fast-Api/app.py
+++ Fast-Api/app.py
@@ -1,9 +1,9 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 #from typing import Union
 from typing import List
 #from pydantic import BaseModel
 from models import User, Role
-from uuid import UUID,uuid4
+from uuid import UUID #,uuid4
 
 app = FastAPI()
 
@@ -45,11 +45,11 @@ async def get_users():
     return db;
 
 @app.get("/api/users/{id}")
-async def get_users(id: UUID):
+async def get_user(id: UUID):
     for user in db:
         if user.id == id:
             return user
-    return {"message": "Usuário não encontrado!"}
+    raise HTTPException(status_code=404, detail="Usuário não encontrado!")
 
 @app.post("/api/users")
 async def add_user(user: User):
@@ -62,7 +62,21 @@ async def del_user(id: UUID):
         if user.id == id:
             db.remove(user)
             return
-    return {"message": "Usuário não encontrado!"}
+    raise HTTPException(
+        status_code=404,
+        detail=f"Usuário com o id{id} não encontrado!"
+    )
+
+@app.put("/api/users/{id}")
+async def update_user(id: UUID, updated_user: User):
+    for index, user in enumerate(db):
+        if user.id == id:
+            db[index] = updated_user
+            return {"message": "Usuário atualizado com sucesso!"}
+    raise HTTPException(
+        status_code=404,
+        detail=f"Usuário com o id {id} não encontrado!"
+    )
 
 #@app.get("/itens/{item_id}")
 #async def read_item(item_id: int, busca: Union[str,int] = None):
